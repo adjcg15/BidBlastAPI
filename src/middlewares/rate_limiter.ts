a3edcdf54e63cddac5b168bc6bc670661bd10178
Angel Garcia <adjcg15@gmail.com>
2024-04-30 17:12:01 -0600

    feat(login middleware): creates RateLimiter classs and implements rate limit in login endpoint

---
+++ src/middlewares/rate_limiter.ts
@@ -0,0 +1,18 @@
+import { rateLimit } from "express-rate-limit";
+
+export default class RateLimiter {
+    private static PUBLIC_ENDPOINT_WINDOW_MINUTES = 15;
+
+    public static limitPublicEndpointUse() {
+        return rateLimit({
+            windowMs: RateLimiter.PUBLIC_ENDPOINT_WINDOW_MINUTES * 60 * 1000,
+            limit: 15,
+            standardHeaders: 'draft-7', 
+            legacyHeaders: true,
+            message: JSON.stringify({
+                error: true,
+                details: `Too many request, try again after ${RateLimiter.PUBLIC_ENDPOINT_WINDOW_MINUTES} minutes.`
+            }) 
+        });
+    }
+}
