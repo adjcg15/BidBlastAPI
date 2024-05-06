e939a0194e992f1f0bbaf108e907a5ee38de0bd5
Angel Garcia <adjcg15@gmail.com>
2024-05-06 14:59:39 -0600

    feat(db.ts): modifies DataBase class to include sequelize connection

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,26 +1,48 @@
-import mysql from "mysql2/promise";
+import { SQLException } from "@exceptions/services";
+import { Sequelize } from "sequelize";
 
 class DataBase {
-    private static POOL_CONNECTION: mysql.Pool | null;
+    private static instance: DataBase;
+    private connection: Sequelize;
 
-    public static connection() {
-        if(DataBase.POOL_CONNECTION == null) {
-            const host = process.env.DB_HOST;
-            const port = parseInt(process.env.DB_PORT ?? "");
-            const database = process.env.DB_NAME;
-            const user = process.env.DB_USER;
-            const password = process.env.DB_PASSWORD;
+    private constructor() {
+        const host = process.env.DB_HOST;
+        const port = parseInt(process.env.DB_PORT ?? "");
+        const database = process.env.DB_NAME ?? "";
+        const user = process.env.DB_USER ?? "";
+        const password = process.env.DB_PASSWORD;
 
-            DataBase.POOL_CONNECTION = mysql.createPool({
-                host,
-                port,
-                database,
-                user,
-                password
-            });
+        this.connection = new Sequelize(database, user, password, {
+            host,
+            dialect: "mysql",
+            dialectOptions: {
+                port
+            }
+        });
+    }
+
+    public static getInstance(): DataBase {
+        if(!DataBase.instance) {
+            DataBase.instance = new DataBase();
         }
-        
-        return DataBase.POOL_CONNECTION;
+
+        return DataBase.instance;
+    }
+
+    public getConnection() {
+        return this.connection;
+    }
+
+    public async start() {
+        try {
+            await this.connection.authenticate();
+        } catch (error) {
+            throw new SQLException("It was not possible to stablish the connection");
+        }
+    }
+
+    public async end() {
+        this.connection.close();
     }
 }
 
