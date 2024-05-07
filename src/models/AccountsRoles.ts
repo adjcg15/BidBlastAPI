4bd2e25ae0a83f7f18fe1418b6402a78ff8f1ff0
Angel Garcia <adjcg15@gmail.com>
2024-05-06 20:26:25 -0600

    feat(AccountsRoles.ts): implements model to represent many-to-many relationship table between accounts and roles

---
+++ src/models/AccountsRoles.ts
@@ -0,0 +1,44 @@
+import DataBase from "@lib/db";
+import { DataTypes, Model } from "sequelize";
+import { UserRoles } from "@ts/enums";
+import Account from "./Account";
+import Role from "./Role";
+
+class AccountsRoles extends Model {
+    declare id_account_role: number;
+    declare name: UserRoles;
+}
+
+AccountsRoles.init(
+    {
+        id_account_rol: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        id_account: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Account,
+                key: "id_account"
+            }
+        },
+        id_rol: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: Role,
+                key: "id_rol"
+            }
+        }
+    },
+    {
+        sequelize: DataBase.getInstance().getConnection(),
+        tableName: "accounts_roles",
+        timestamps: false
+    }
+);
+
+Role.belongsToMany(Account, { through: AccountsRoles });
+Account.belongsToMany(Role, { through: AccountsRoles });
+
+export default Role;
