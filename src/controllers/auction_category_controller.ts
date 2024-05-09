71b4c96420c9f7ca92964fc0656848c42dc5661d
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-08 21:04:58 -0600

    feat(update auction category): implements update auction category endpoint

--- src/controllers/auction_category_controller.ts
+++ src/controllers/auction_category_controller.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import Logger from "@lib/logger";
 import AuctionCategoryService from "services/auction_category_service";
 import { DataContextException } from "@exceptions/services";
-import { Console } from "winston/lib/winston/transports";
+import { error } from "winston";
 
 class AuctionCategoryController{
     public static async getAuctionCategory(req: Request, res: Response): Promise<void> {
@@ -34,6 +34,47 @@ class AuctionCategoryController{
             if(error instanceof DataContextException) {
                 Logger.error(error.name, error.message);
                 responseDetails.details = "It was not possible to recover category, please try it again later";
+            } else {
+                Logger.error(error.name, error.message);
+            }
+
+            res.status(statusCode).json(responseDetails);
+        }
+    }
+
+    public static async updateAuctionCategory(req: Request, res: Response): Promise<void> {
+        try {
+            const { catid } = req.params;
+            const idCategory: number = parseInt(catid);
+
+            const { title, description, keywords } = req.body;
+
+            const isUpdated = await AuctionCategoryService.updateAuctionCategory(idCategory, title, description, keywords);
+            if(!isUpdated){
+                res.status(HttpStatusCodes.BAD_REQUEST).json({
+                    error: true,
+                    statusCode: HttpStatusCodes.BAD_REQUEST,
+                    details: "verify that the id exists and the title is unique"
+                });
+                return;
+            }
+
+            res.status(HttpStatusCodes.CREATED).json({
+                error: false,
+                statusCode: HttpStatusCodes.CREATED,
+                details: "Auction category is updated"
+            });
+        } catch (error: any) {
+            let statusCode = HttpStatusCodes.INTERNAL_SERVER_ERROR;
+            const responseDetails = {
+                error: true,
+                statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+                details: "There was an unexpeted error, please try it again later"
+            };
+
+            if(error instanceof DataContextException) {
+                Logger.error(error.name, error.message);
+                responseDetails.details = "It was not possible to update category, please try it again later";
             } else {
                 Logger.error(error.name, error.message);
             }
--- src/request_schemas/auction_category_request_validator.ts
+++ src/request_schemas/auction_category_request_validator.ts
@@ -10,6 +10,34 @@ class AuctionCategoriesRequestValidator {
             },
         };
     }
+
+    public static changeAuctionCategorySchema(): Schema {
+        return {
+            catid: {
+                in: ["params"],
+                isNumeric: true,
+                errorMessage: "Invalid category id"
+            },
+            title: {
+                in: ["body"],
+                trim: true,
+                notEmpty: true,
+                errorMessage: "Title is required"
+            },
+            description: {
+                in: ["body"],
+                trim: true,
+                notEmpty: true,
+                errorMessage: "Description is required"
+            },
+            keywords:{
+                in: ["body"],
+                trim: true,
+                notEmpty: true,
+                errorMessage: "Keywords is required"
+            }
+        };
+    }
 }
 
 export default AuctionCategoriesRequestValidator;
