1e5a3fcb8b35e0008ee4527ebc58bca7c1639cdf
Angel Garcia <adjcg15@gmail.com>
2024-05-10 11:22:16 -0600

    feat(auction_routes.ts): implements token validation middleware to search auction endpoint

--- src/routes/auction_routes.ts
+++ src/routes/auction_routes.ts
@@ -1,9 +1,11 @@
 import AuctionController from "@controllers/auction_controller";
+import AccessControl from "@middlewares/access_control";
 import { Router } from "express";
 
 const AuctionRouter = Router();
 
 AuctionRouter.get("/",
+    AccessControl.checkTokenValidity,
     AuctionController.searchAuction);
 
 export default AuctionRouter;
