197ed2c27b068b718ca254236402cd26c3d7b1b1
Angel Garcia <adjcg15@gmail.com>
2024-05-10 11:19:45 -0600

    feat(search auction): creates search auction controller in AuctionController class and exposes it in auction routes

---
+++ src/controllers/auction_controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+class AuctionController {
+    public static async searchAuction(req: Request, res: Response) {
+        res.send();
+    }
+}
+
+export default AuctionController;
--- src/routes/auction_routes.ts
+++ src/routes/auction_routes.ts
@@ -1,5 +1,9 @@
+import AuctionController from "@controllers/auction_controller";
 import { Router } from "express";
 
 const AuctionRouter = Router();
+
+AuctionRouter.get("/",
+    AuctionController.searchAuction);
 
 export default AuctionRouter;
