5e9d11bf1bb285089f551032cc7d747fc8858252
Angel Garcia <adjcg15@gmail.com>
2024-05-10 12:40:34 -0600

    feat(data.ts): defines data types for auction and hypermedia file

--- src/types/data.ts
+++ src/types/data.ts
@@ -3,21 +3,44 @@ import { UserRoles } from "./enums";
 interface IUserData {
     id: number;
     fullName: string;
-    phoneNumber: string | null;
-    avatar: string;
-    email: string;
+    phoneNumber?: string | null;
+    avatar?: string;
+    email?: string;
     password?: string;
-    roles: UserRoles[];
+    roles?: UserRoles[];
 }
 
 interface IAuctionCategory {
     id: number;
     title: string;
-    description: string;
-    keywords: string;
+    description?: string;
+    keywords?: string;
+}
+
+interface IHypermediaFileData {
+    id: number;
+    content: string;
+    mimeType?: string;
+    name?: string;
+}
+
+interface IAuctionData {
+    id: number;
+    title: string;
+    description?: string;
+    basePrice?: number;
+    minimumBid?: number;
+    approvalDate?: Date;
+    closesAt?: Date;
+    daysAvailable?: number;
+    category?: IAuctionCategory;
+    auctioneer?: IUserData;
+    mediaFiles?: IHypermediaFileData[];
 }
 
 export type {
     IUserData,
-    IAuctionCategory
+    IAuctionCategory,
+    IAuctionData,
+    IHypermediaFileData
 }
