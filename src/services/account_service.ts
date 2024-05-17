9fcdb3d1e6be1bfcc1b0547988234e878c7fbd41
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-05-16 19:20:37 -0600

    feat(account_service): Implements the service for creating an account with an associated profile

---
+++ src/services/account_service.ts
@@ -0,0 +1,44 @@
+import Account from "@models/Account";
+import Profile from "@models/Profile";
+import { DataContextException } from "@exceptions/services";
+import { Transaction } from "sequelize";
+
+class AccountService {
+    public static async createAccount(fullName: string, email: string, phoneNumber: string | null, avatar: Buffer | null, password: string): Promise<Account> {
+        let transaction: Transaction | null = null;
+
+        try {
+            const existingAccount = await Account.findOne({ where: { email } });
+            if (existingAccount) {
+                throw new Error("Email already exists");
+            }
+            if (!Account.sequelize) {
+                throw new DataContextException("Sequelize instance is not available");
+            }
+
+            transaction = await Account.sequelize.transaction();
+
+            const account = await Account.create(
+                { email, password },
+                { transaction }
+            );
+
+            await Profile.create(
+                { full_name: fullName, phone_number: phoneNumber, avatar: avatar, id_account: account.id_account },
+                { transaction }
+            );
+            await transaction.commit();
+
+            return account;
+        } catch (error: any) {
+
+            if (transaction) await transaction.rollback();
+
+            if (error.message === "Email already exists") {
+                throw new Error("Email already exists");
+            }
+            throw new DataContextException("Error while creating account");
+        }
+    }
+}
+export default AccountService;
