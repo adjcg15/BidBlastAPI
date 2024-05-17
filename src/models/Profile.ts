54b5c9bac5bb67cef1d67b8766e7464c12a9252b
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-05-16 19:05:59 -0600

    fix(Profile): Modifies the profile model to be able to add the relationship between the profile model and account

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -1,11 +1,13 @@
 import DataBase from "@lib/db";
 import { DataTypes, Model } from "sequelize";
+import Account from "@models/Account";
 
 class Profile extends Model {
     declare id_profile: number;
     declare full_name: string;
-    declare phone_number: string;
-    declare avatar: Buffer;
+    declare phone_number: string | null;
+    declare avatar: Buffer | null;
+    declare id_account: number;
 }
 
 Profile.init(
@@ -26,6 +28,14 @@ Profile.init(
         avatar: {
             type: DataTypes.BLOB,
             allowNull: true
+        },
+        id_account: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Account,
+                key: 'id_account'
+            }
         }
     },
     {
@@ -34,5 +44,9 @@ Profile.init(
         timestamps: false
     }
 );
+
+// Definir la relación entre Account y Profile
+Account.hasOne(Profile, { foreignKey: 'id_account' });
+Profile.belongsTo(Account, { foreignKey: 'id_account' });
 
 export default Profile;
