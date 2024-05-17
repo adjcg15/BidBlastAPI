533ef42b4373f5d1583ca4aef8bf32e354a18ef8
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-05-17 10:02:46 -0600

    feat(account_controller): Creates the driver required for account creation

---
+++ src/controllers/account_controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import AccountService from "services/account_service";
+import { DataContextException } from "@exceptions/services";
+import { HttpStatusCodes } from "@ts/enums";
+
+class AccountController {
+    public static async createAccount(req: Request, res: Response): Promise<void> {
+        
+        const { fullName, email, phoneNumber, avatar, password } = req.body;
+
+        try {
+            const avatarBuffer = avatar ? Buffer.from(avatar, 'base64') : null;
+            const account = await AccountService.createAccount(fullName, email, phoneNumber, avatarBuffer, password);
+            res.status(HttpStatusCodes.CREATED).json({ message: "Account created successfully", account });
+        } catch (error: any) {
+            console.error("Error type:", error.constructor.name);
+            console.error("Error message:", error.message);
+
+            let statusCode = HttpStatusCodes.INTERNAL_SERVER_ERROR;
+            const responseDetails = {
+                error: true,
+                statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+                details: "There was an unexpected error, please try again later"
+            };
+
+            if (error instanceof DataContextException) {
+                responseDetails.details = "It was not possible to create the account, please try again later";
+            } else if (error.message === "Email already exists") {
+                statusCode = HttpStatusCodes.BAD_REQUEST;
+                responseDetails.details = "Email already exists";
+            }
+
+            res.status(statusCode).json(responseDetails);
+        }
+    }
+}
+
+export default AccountController;
