12c41f00f6132b6e83ec3661a049403ff4ecfc4c
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-20 00:32:03 -0600

    feat(user_request_validator): implements user on black list schema

--- src/request_schemas/user_request_validator.ts
+++ src/request_schemas/user_request_validator.ts
@@ -58,6 +58,23 @@ class UserRequestValidator {
             }
         };
     }
+
+    public static userOnBlackListSchema() : Schema{
+        return {
+            id_profile: {
+                in: ["body"],
+                notEmpty: true,
+                isInt: true,
+                errorMessage: "Invalid profile id"
+            },
+            id_pauction: {
+                in: ["body"],
+                notEmpty: true,
+                isInt: true,
+                errorMessage: "Invalid auction id"
+            }
+        }
+    }
 }
 
 export default UserRequestValidator;
