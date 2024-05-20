7fb695ab6dbdbe4dc2c861eb0c2e312cb319219a
Angel Garcia <adjcg15@gmail.com>
2024-05-20 16:51:36 -0600

    fix(app routes): modifies routes distribution to support swagger

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
 
-import SessionRouter from "@routes/session_routes";
-import AuctionCategoryRouter from "@routes/auction_category_routes";
 import DataBase from "@lib/db";
 import configureModel from "@models/associations";
-import AuctionRouter from "@routes/auction_routes";
-import AccountRouter from "@routes/account_routes";
-import UserRouter from "@routes/user_routes";
 import { swaggerDocs } from "swagger";
+import MainRouter from "./routes";
 
 const database = DataBase.getInstance();
 database.startConnection()
@@ -21,11 +17,7 @@ database.startConnection()
 
         app.use(express.json());
         app.use(cors());
-        app.use("/api/sessions", SessionRouter);
-        app.use("/api/auction-categories", AuctionCategoryRouter);
-        app.use("/api/auctions", AuctionRouter);
-        app.use("/api/accounts", AccountRouter);
-        app.use("/api/users", UserRouter);
+        app.use(MainRouter);
 
         app.listen(process.env.PORT, () => {
             console.log(`Server is running on ${process.env.HOST_URL}:${APP_PORT}`);
--- src/swagger.ts
+++ src/swagger.ts
@@ -1,25 +1,9 @@
 import { Application } from "express";
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-
-const options: swaggerJSDoc.Options = {
-    definition: {
-        openapi: "3.0.0",
-        info: { title: "BidBlast API", version: "1.0.0" }
-    },
-    apis: [
-        "src/routes/account_routes.ts",
-        "src/routes/auction_category_routes.ts",
-        "src/routes/auction_routes.ts",
-        "src/routes/session_routes.ts",
-        "src/routes/user_routes.ts",
-    ]
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+import swaggerFile from "../swagger-output.json";
 
 const swaggerDocs = (app: Application, port?: string) => {
-    app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
     console.log(`Docs are available on ${process.env.HOST_URL}:${port}/api/docs`)
 };
 
