982ce9c68eee2d63de9d2cc68842f1e6e267e86c
Angel Garcia <adjcg15@gmail.com>
2024-05-20 22:36:20 -0600

    feat(offer_controller.ts): prepares endpoint for consuming service that creates a new offer

--- src/controllers/offer_controller.ts
+++ src/controllers/offer_controller.ts
@@ -1,8 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 class OfferController {
-    public static async createOffer(req: Request, res: Response): Promise<void> {
-        res.send();
+    public static async createOffer(req: Request, res: Response, next: NextFunction): Promise<void> {
+        try {
+            const { auctionId, amount } = req.body as { auctionId: number, amount: number };
+            
+            res.send();
+        } catch (error) {
+            next(error);
+        }
     }
 }
 
