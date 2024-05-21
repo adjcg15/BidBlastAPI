e083e7f2332a67795397bcaf91519d2e4fdeb6ff
Angel Garcia <adjcg15@gmail.com>
2024-05-20 20:55:38 -0600

    feat(access_control.ts): modifies checkTokenValidity method to handle token refresh

--- src/middlewares/access_control.ts
+++ src/middlewares/access_control.ts
@@ -3,20 +3,31 @@ import { HttpStatusCodes, UserRoles } from "@ts/enums";
 import { NextFunction, Request, Response } from "express";
 
 class AccessControl {
+    private static readonly TOKEN_RENEWAL_LIMIT = 60 * 5;
+
     public static checkTokenValidity(req: Request, res: Response, next: NextFunction): void {
         const authorizationHeader = String(req.headers.authorization);
-        if(!authorizationHeader) {
+        if(!authorizationHeader.startsWith("Bearer ")) {
             res.status(HttpStatusCodes.UNAUTHORIZED).send();
             return;
         }
 
-        const token = authorizationHeader.substring(authorizationHeader.indexOf(' ') + 1);
+        const token = authorizationHeader.split(' ')[1];
         const tokenStore = new TokenStore();
         const payload = tokenStore.verify(token); 
 
         if(!payload) {
             res.status(HttpStatusCodes.UNAUTHORIZED).send();
             return;
+        }
+
+        const tokenValiditySeconds = (payload.exp ?? 0) - (new Date().getTime() / 1000);
+        if(tokenValiditySeconds < AccessControl.TOKEN_RENEWAL_LIMIT) {
+            const { id, email, userRoles } = payload;
+            const tokenStore = new TokenStore();
+            const newToken = tokenStore.sign({ id, email, userRoles });
+
+            res.header("Set-Authorization", newToken);
         }
 
         req.user = payload;
