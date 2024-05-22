7814046b3400c94ca014f336f21b29f826d4a2c1
Angel Garcia <adjcg15@gmail.com>
2024-05-21 18:27:34 -0600

    feat(offer_controller.ts): implements connection to create offer service

--- src/controllers/offer_controller.ts
+++ src/controllers/offer_controller.ts
@@ -1,11 +1,35 @@
+import { CreateOfferCodes, HttpStatusCodes } from "@ts/enums";
 import { NextFunction, Request, Response } from "express";
+import OfferService from "services/offer_service";
 
 class OfferController {
     public static async createOffer(req: Request, res: Response, next: NextFunction): Promise<void> {
         try {
             const { auctionId, amount } = req.body as { auctionId: number, amount: number };
+            const { id: userId } = req.user;
             
-            res.send();
+            const offerCreationResult = await OfferService.createOffer(auctionId, userId, amount);
+            if(offerCreationResult !== null) {
+                const errorMessages = {
+                    [CreateOfferCodes.AUCTION_BLOCKED]: "You are not able to make offers to this auction. The auctioneer has blocked you",
+                    [CreateOfferCodes.AUCTION_FINISHED]: "This auction is closed, it can't receive more offers",
+                    [CreateOfferCodes.AUCTION_NOT_FOUND]: `The auction with ID ${auctionId} does not exists`,
+                    [CreateOfferCodes.BASE_PRICE_NOT_FULLFILLED]: "Your offer is lower than the base auction price",
+                    [CreateOfferCodes.EARLY_OFFER]: "You must wait before making an offer again",
+                    [CreateOfferCodes.MINIMUM_BID_NOT_FULFILLED]: "Your offer is lower than the minimum bid allowed for this auction",
+                    [CreateOfferCodes.OFFER_OVERCOMED]: "There is a better offer for this auction, improve yours"
+                }
+
+                res.status(HttpStatusCodes.BAD_REQUEST).json({
+                    error: true,
+                    statusCode: HttpStatusCodes.BAD_REQUEST,
+                    details: errorMessages[offerCreationResult],
+                    apiErrorCode: offerCreationResult
+                });
+                return;
+            }
+
+            res.status(HttpStatusCodes.CREATED).send();
         } catch (error) {
             next(error);
         }
