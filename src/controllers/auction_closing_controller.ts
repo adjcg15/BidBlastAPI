a1d7c5a4630a53cf66f1a633138defc403093e4e
Angel Garcia <adjcg15@gmail.com>
2024-05-22 19:10:12 -0600

    feat(cuction_closing_controller): implements the logic to close auctions and notify the actuioneer and customer about the sale

--- src/controllers/auction_closing_controller.ts
+++ src/controllers/auction_closing_controller.ts
@@ -1,9 +1,25 @@
+import EmailService from "@lib/email_service";
+import { HttpStatusCodes } from "@ts/enums";
 import { NextFunction, Request, Response } from "express";
+import AuctionService from "services/auction_service";
 
 class AuctionClosingController {
     public static async closeAuctions(req: Request, res: Response, next: NextFunction) {
         try {
-            res.send();
+            const expiredAuctions = await AuctionService.getExpiredAuctions();
+
+            for(const auction of expiredAuctions) {
+                if(!auction.lastOffer) {
+                    await AuctionService.closeAuction(auction.id);
+                } else {
+                    await AuctionService.concretizeAuction(auction.id);
+                    const emailService = new EmailService();
+                    await emailService.notifySaleToAuctioneer(auction);
+                    await emailService.notifySaleToCustomer(auction);
+                }
+            }
+
+            res.status(HttpStatusCodes.CREATED).send();
         } catch(error: any) {
             next(error);
         }
