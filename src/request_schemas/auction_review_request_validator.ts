7e364d6cdefa1900caa981a9ab4fc1c4752e682a
Angel Garcia <adjcg15@gmail.com>
2024-05-24 10:48:18 -0600

    feat(auction review endpoint): implements body validation to include idAuction

--- src/request_schemas/auction_review_request_validator.ts
+++ src/request_schemas/auction_review_request_validator.ts
@@ -3,6 +3,14 @@ import { Schema } from "express-validator";
 class AuctionReviewRequestValidator {
     public static auctionReviewSchema(): Schema {
         return {
+            idAuction: {
+                in: ["body"],
+                isInt: {
+                    options: { min: 1 },
+                    errorMessage: "The body attribute 'idAuction' is an invalid ID for an auction"
+                },
+                toInt: true
+            },
             isApproved: {
                 in: ["body"],
                 isBoolean: true,
