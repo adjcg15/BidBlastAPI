e36427b02fb2b2804c8222a7aef87ecacd1e9d71
Angel Garcia <adjcg15@gmail.com>
2024-05-24 13:30:40 -0600

    feat(auction_review_controller.ts): connects create auction review endpoint with services

--- src/controllers/auction_review_controller.ts
+++ src/controllers/auction_review_controller.ts
@@ -1,9 +1,23 @@
+import { HttpStatusCodes } from "@ts/enums";
 import { NextFunction, Request, Response } from "express";
+import AuctionReviewService from "services/auction_review_service";
+import AuctionService from "services/auction_service";
 
 class AuctionReviewController {
     public static async createAuctionReview(req: Request, res: Response, next: NextFunction) {
         try {
-            res.send();
+            const { isApproved, idAuction, comments } = req.body;
+            const { id: idModerator } = req.user;
+
+            if(isApproved) {
+                await AuctionService.publishAuction(idAuction);
+                await AuctionService.convertAuctionAuthorToAuctioneer(idAuction);
+            } else {
+                await AuctionService.rejectAuction(idAuction);
+                await AuctionReviewService.createAuctionReview(comments, idAuction, idModerator);
+            }
+
+            res.status(HttpStatusCodes.CREATED).send();
         } catch(error) {
             next(error);
         }
