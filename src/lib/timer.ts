ddc359438f6e9b396a5b04c437aff8db2fe10158
Angel Garcia <adjcg15@gmail.com>
2024-05-27 17:28:57 -0600

    feat(auctions closing): implements logic for auctions closing every five minutes

--- src/lib/timer.ts
+++ src/lib/timer.ts
@@ -1,9 +1,20 @@
+import AuctionClosingService from "services/auction_closing_service";
+import Logger from "./logger";
+
 class Timer {
     public static startAuctionsClosingListener() {
         const EXECUTION_INTERVAL = 1000 * 60 * 5;
 
-        setInterval(() => {
-            console.log("Hola mundo");
+        const closeAuctions = () => {
+            AuctionClosingService.closeAuctions()
+                .catch((error: any) => {  
+                    Logger.error(error.name, "Error closing auctions. " + error.message);
+                });
+        };
+
+        setTimeout(() => {
+            closeAuctions();
+            setInterval(closeAuctions, EXECUTION_INTERVAL);
         }, EXECUTION_INTERVAL);
     }
 }
