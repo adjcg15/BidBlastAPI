848a68e063d6a9b2ef317eae32524282ab6808cd
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-06-04 03:32:19 -0600

    fix(account_controller): changes error handling to use centralized error handler

--- src/controllers/account_controller.ts
+++ src/controllers/account_controller.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import AccountService from "services/account_service";
 import { DataContextException } from "@exceptions/services";
 import { HttpStatusCodes } from "@ts/enums";
 
 class AccountController {
-    public static async createAccount(req: Request, res: Response): Promise<void> {
+    public static async createAccount(req: Request, res: Response, next: NextFunction): Promise<void> {
         /*
             #swagger.tags = ['Accounts']
             #swagger.summary = 'Creates a new account'
@@ -64,32 +64,28 @@ class AccountController {
                 }
             }
         */
-        const { fullName, email, phoneNumber, avatar, password } = req.body;
-
         try {
+            const { fullName, email, phoneNumber, avatar, password } = req.body;
+
             const avatarBuffer = avatar ? Buffer.from(avatar, 'base64') : null;
             const account = await AccountService.createAccount(fullName, email, phoneNumber, avatarBuffer, password);
             res.status(HttpStatusCodes.CREATED).json({ message: "Account created successfully", account });
         } catch (error: any) {
-            console.error("Error type:", error.constructor.name);
-            console.error("Error message:", error.message);
-
-            let statusCode = HttpStatusCodes.INTERNAL_SERVER_ERROR;
-            const responseDetails = {
-                error: true,
-                statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
-                details: "There was an unexpected error, please try again later"
-            };
-
-            if (error instanceof DataContextException) {
-                responseDetails.details = "It was not possible to create the account, please try again later";
-            } else if (error.message === "Email already exists") {
-                statusCode = HttpStatusCodes.UNAUTHORIZED;
-                responseDetails.statusCode = statusCode;
-                responseDetails.details = "The email address is already in use. Please use a different email address.";
+            if (error.message === "Email already exists") {
+                res.status(HttpStatusCodes.UNAUTHORIZED).json({
+                    error: true,
+                    statusCode: HttpStatusCodes.UNAUTHORIZED,
+                    details: "The email address is already in use. Please use a different email address."
+                });
+            } else if (error instanceof DataContextException) {
+                res.status(HttpStatusCodes.BAD_REQUEST).json({
+                    error: true,
+                    statusCode: HttpStatusCodes.BAD_REQUEST,
+                    details: "It was not possible to create the account, please try again later"
+                });
+            } else {
+                next(error);
             }
-
-            res.status(statusCode).json(responseDetails);
         }
     }
 }
